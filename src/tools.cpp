f6d170acb18d7c4e0b626a296f095b537ccd30a3
zhao-zh10 <46366974+zhao-zh10@users.noreply.github.com>
2019-03-29 13:30:33 +0800

    project initial files

---
+++ src/tools.cpp
@@ -0,0 +1,15 @@
+#include "tools.h"
+
+using Eigen::VectorXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+   * TODO: Calculate the RMSE here.
+   */
+}
